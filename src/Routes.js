a2371cc52ebf121071b660dca5901bb694df60b2
Ray Vafa <vafatabar@gmail.com>
2017-04-09 21:04:14 +1000

    Create a Login Page

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NotFound from './containers/NotFound';
+import Login from './containers/Login';
 
 import Home from './containers/Home';
 
 export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>
