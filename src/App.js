8b8ecdb38b693c38ae06e011bfba61757d862a80
Ray Vafa <vafatabar@gmail.com>
2017-04-09 21:32:39 +1000

    Complete login functionality by storing userToken in App.js And displaying a logout button in nav bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import {
   Nav,
+  NavItem,
   Navbar
 } from 'react-bootstrap';
 import RouteNavItem from './components/RouteNavItem';
@@ -13,12 +14,36 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userToken: null,
+    };
+  }
+
+  updateUserToken = (userToken) => {
+    this.setState({
+      userToken: userToken
+    });
+  }
+
   handleNavLink = (event) => {
     event.preventDefault();
     this.props.history.push(event.currentTarget.getAttribute('href'));
   }
 
+  handleLogout = (event) => {
+    this.updateUserToken(null);
+  }
+
   render() {
+
+    const childProps = {
+      userToken: this.state.userToken,
+      updateUserToken: this.updateUserToken,
+    };
+
     return (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
@@ -30,12 +55,15 @@ class App extends Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <RouteNavItem onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>
-              <RouteNavItem onClick={this.handleNavLink} href="/login">Login</RouteNavItem>
+              { this.state.userToken
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : [ <RouteNavItem key={1} onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>,
+                  <RouteNavItem key={2} onClick={this.handleNavLink} href="/login">Login</RouteNavItem> ]
+              }
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Routes />
+        <Routes childProps={childProps} />
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
-
+import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 
-export default () => (
+export default ({ childProps }) => (
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/login" exact component={Login} />
-    { /* Finally, catch all unmatched routes */ }
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <Route component={NotFound} />
   </Switch>
 );
