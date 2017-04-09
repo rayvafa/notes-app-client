a537459e94746116ab12b1ab65bfba78341dad99
Ray Vafa <vafatabar@gmail.com>
2017-04-09 22:43:15 +1000

    Add the “create note” page

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
 import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 
@@ -11,6 +12,7 @@ export default ({ childProps }) => (
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <Route component={NotFound} />
   </Switch>
 );
